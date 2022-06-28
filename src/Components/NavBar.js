e7934647b05037ecd3be5fa17de3d0c9623964da
saadali <saad.ali234@gmail.com>
2022-06-28 14:14:52 +0530

    updated navbar and banner

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div style={{ display: "flex", padding: '0.5' }}>
+                <h1 style={{ marginTop: '1rem', marginLeft: '1rem' }}>
+                    Movies App
+                </h1>
+                <h2 style={{ marginTop: '1.4rem', marginLeft: '1.5rem' }}>
+                    Favourites
+                </h2>
+            </div>
+        )
+    }
+}
